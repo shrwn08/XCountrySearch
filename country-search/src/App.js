6bc4988555f3ca81fd029f047e4b7cdb27546aba
shrwn08 <shrwnkumar08@gmail.com>
2024-06-25 02:05:35 +0530

    completed

---
+++ country-search/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CountrySearch from "./Components/CountrySearch";
+import "./App.css";
+
+function App() {
+  const [countryData, setCountryData] = useState([]);
+  const [query,setQuery]=useState('');
+
+  const handleSeachBar = (event) =>{
+    setQuery(event.target.value)
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://restcountries.com/v3.1/all");
+        console.log(response.data);
+        setCountryData(response.data)
+      } catch (error) {
+        console.log('unable to fetchData', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <CountrySearch data={countryData} handleSeachBar={handleSeachBar}
+        query={query}
+      />
+    </div>
+  );
+}
+
+export default App;
